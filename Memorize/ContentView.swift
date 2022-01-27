7135d360d0b593157df96eb97a9a68b429f5c8bb
Gwendolyn Friedman <gwenrfriedman@gmail.com>
2022-01-27 18:04:16 -0500

    lecture 4

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,17 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    // type could  be Array<String> or [String]
-    
-    var vehicleEmojis = ["🚲", "🚚", "🛴", "✈️", "🚘", "🛳", "🚀", "🚁"]
-    var animalEmojis = ["🐶", "🐱", "🐭", "🦊", "🐼", "🐤", "🐸", "🐌", "🐝", "🐍", "🦀"]
-    
-    var flagEmojis = ["🏴‍☠️", "🏳️‍🌈", "🇨🇦", "🇬🇷", "🇯🇵", "🇳🇱", "🇺🇸", "🏁", "🇮🇱", "🇵🇼", "🏴󠁧󠁢󠁥󠁮󠁧󠁿", "🇦🇸", "🇮🇪"]
-    
-    @State var emojis = ["🚲", "🚚", "🛴", "✈️", "🚘", "🛳", "🚀", "🚁"]
-    
-    
-    @State var emojiCount: Int = 8
+    //when something changes, update UI
+    @ObservedObject var viewModel: EmojiMemoryGame
     
     var body: some View {
         
@@ -27,112 +18,55 @@ struct ContentView: View {
                 .font(.title)
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
-                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
-                        CardView(content: emoji)
+                    ForEach(viewModel.cards) { card in
+                        CardView(card: card)
                             .aspectRatio( 2/3, contentMode: .fit)
+                            .onTapGesture {
+                                //Intent to choose a card
+                                viewModel.choose(card)
+                            }
                 
                     }
                 }
             }
             .foregroundColor(/*@START_MENU_TOKEN@*/.red/*@END_MENU_TOKEN@*/)
             Spacer(minLength: 20)
-            HStack{
-                VStack{
-                    vehicle
-                    Text("vehicles")
-                        .font(.body)
-                        .foregroundColor(Color.blue)
-                }
-                
-                //will grab as much room as it can
-                Spacer()
-                
-                VStack{
-                    animal
-                    Text("animals")
-                        .font(.body)
-                        .foregroundColor(Color.blue)
-                }
-                
-                Spacer()
-                
-                VStack{
-                    flag
-                    Text("flags")
-                        .font(.body)
-                        .foregroundColor(Color.blue)
-                }
-                
-            }
             .padding(.horizontal)
             .font(.largeTitle)
         }
         .padding(.horizontal)
             
     }
-    
-    var vehicle: some View {
-        Button {
-            emojis = vehicleEmojis.shuffled()
-            emojiCount = emojis.count
-        } label: {
-            Image(systemName: "car")
-        }
-    }
-    
-    var animal: some View {
-        Button {
-            emojis = animalEmojis.shuffled()
-            emojiCount = emojis.count
-        } label: {
-            Image(systemName: "hare")
-        }
-    }
-    
-    var flag: some View {
-        Button {
-            emojis = flagEmojis.shuffled()
-            emojiCount = emojis.count
-        } label: {
-            Image(systemName: "flag")
-        }
-    }
 }
 
 
 struct CardView: View {
-    
-    //if default value is set, the passed value will override it
-    //vars are immutable, add @State
-    //@State makes it a pointer to a var somewhere else
-    @State var isFaceUp: Bool = true
-    var content: String
-    
+    let card: MemoryGame<String>.Card
     var body: some View {
         //Don't need the parentheses or the label because it's a function
         ZStack {
             let shape = RoundedRectangle(cornerRadius: 20)
-            if isFaceUp {
+            if card.isFaceUp {
             //no way to stroke and fill a rounded rectangle
                 shape.fill().foregroundColor(.white)
                 shape.strokeBorder(lineWidth: 3)
-                Text(content).font(.largeTitle)
+                Text(card.content).font(.largeTitle)
+            } else if card.isMatched {
+                shape.opacity(0)
             } else {
                 shape.fill()
             }
-        }
-        .onTapGesture {
-            isFaceUp = !isFaceUp
         }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        let game = EmojiMemoryGame()
         //You can add more previews here
-        ContentView()
+        ContentView(viewModel: game)
             .preferredColorScheme(.dark)
-        ContentView()
+        ContentView(viewModel: game)
             .preferredColorScheme(.light)
     }
 }
--- Memorize/EmojiMemoryGame.swift
+++ Memorize/EmojiMemoryGame.swift
@@ -10,7 +10,7 @@
 
 import SwiftUI
 
-class EmojiMemoryGame {
+class EmojiMemoryGame : ObservableObject {
     //connection to the model
     //create its own model - not always the case for a ViewModel (ex when it's a database)
     
@@ -23,7 +23,7 @@ class EmojiMemoryGame {
     
     //private - only the view model's code can see the model, protects the model from changes
     //private(set) - can look at the model, but can't change it
-    private var model: MemoryGame<String> = createMemoryGame()
+    @Published private var model: MemoryGame<String> = createMemoryGame()
     
     //if model is private, use this - don't need if private(set)
     var cards: Array<MemoryGame<String>.Card> {
@@ -34,5 +34,12 @@ class EmojiMemoryGame {
 //    func foo() {
 //        let x = EmojiMemoryGame.vehicleEmojis
 //    }
+    
+    
+    // MARK: - Intent(s)
+    
+    func choose(_ card: MemoryGame<String>.Card) {
+        model.choose(card)
+    }
 }
 
